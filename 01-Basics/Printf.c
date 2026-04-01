f138f004cdc7a058695d507b58157127bdc33c1d
Huzaif0211 <huzaifahbinafaq7@gmail.com>
2026-04-01 15:55:30 +0530

    Added printf examples

---
+++ 01-Basics/Printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, Huzaif!\n");
+    printf("Welcome to C programming.");
+
+    return 0;
+}
+
+/*printf prints text and \n helps to move to next line. */
