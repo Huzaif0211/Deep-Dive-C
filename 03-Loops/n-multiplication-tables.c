e6925af514c68ad88da741a6236df66df889b658
Huzaif0211 <huzaifahbinafaq7@gmail.com>
2026-06-12 11:40:18 +0530

    Added a multiple multiplication table generator program

---
+++ 03-Loops/n-multiplication-tables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i, j, n;
+
+    printf("Enter the number you want multiplication table upto ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+       
+        printf("Table of %d\n", i);
+        for (j = 1; j <= 10; j++)
+        {
+            printf("%d * %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
